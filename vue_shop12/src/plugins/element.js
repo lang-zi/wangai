080b0e71269d2728ecac5601f3593d821e70177b
彭博华1997 <beibei54088@qq.com>
2022-05-10 22:13:59 +0800

    wangai

---
+++ vue_shop12/src/plugins/element.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+//导入弹框提示组件  Message需要进行全局挂载
+import { Button,Form, FormItem , Input,Message,Container,Header,Aside,Main ,Menu,Submenu,MenuItem,Breadcrumb,BreadcrumbItem,Card,Row,Col,Table,TableColumn,Switch,Tooltip,Pagination,Dialog,MessageBox,Tag,Tree,Select,Option,Cascader,Alert,Tabs,TabPane,Steps,Step,CheckboxGroup,Checkbox,Upload} from 'element-ui'
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+//用这个注册全局组件
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
+Vue.use(Dialog)
+Vue.use(Tag)
+Vue.use(Tree)
+Vue.use(Select)
+Vue.use(Option)
+Vue.use(Cascader)
+Vue.use(Alert)
+Vue.use(Tabs)
+Vue.use(TabPane)
+Vue.use(Steps)
+Vue.use(Step)
+Vue.use(CheckboxGroup)
+Vue.use(Checkbox)
+Vue.use(Upload)
+
+
+//这个表示把Message挂载到VUE 原型对象上面，这样每个组件都可以通过this访问$message  只要访问到$message就可以进行弹窗提示
+Vue.prototype.$message = Message
+
+Vue.prototype.$confirm = MessageBox.confirm
---
+++ vue_shop12/src/vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    pages:{
+        index:{
+            entry:'src/main.js'
+        }
+    },
+      lintOnSave:false//前提是已经删除了eslint插件
+}
